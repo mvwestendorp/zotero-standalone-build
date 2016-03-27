7d6966c550feb5ebb939cfb11947ec6235769b8d
fbennett <biercenator@gmail.com>
2016-03-27 10:45:55 +0900

    Mess with script, which is broken on Mac

--- build.sh
+++ build.sh
@@ -42,6 +42,17 @@ DONE
 	exit 1
 }
 
+function seq () {
+  if [ "$1" -gt "$2" ] ; then
+    for ((i=$1; i<=$2; i++))
+      do echo $i
+    done
+  else
+    for ((i=$1; i>=$2; i--))
+      do echo $i
+    done
+  fi
+}
 
 
 PACKAGE=1
