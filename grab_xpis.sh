5fc0bd716386e4f392c483d1a9a1cfc9f085e62c
Frank Bennett <biercenator@gmail.com>
2017-08-01 07:21:46 +0900

    Accomomdate gsed for Mac

--- grab_xpis.sh
+++ grab_xpis.sh
@@ -5,6 +5,16 @@
 
 set -e
 
+set +e
+gsed --version > /dev/null 2<&1
+if [ $? -gt 0 ]; then
+    GSED="sed"
+else
+    GSED="gsed"
+fi
+set -e
+
+
 
 CALLDIR="$3"
 if [ "" == "$3" ]; then
@@ -107,7 +117,7 @@ CONTAINER_DIR=$(dirname "$CALLDIR")
 		CL_KEY=$(cat "${CONTAINER_DIR}"/jurism/cl-key.txt)
 		echo $CL_KEY
 		echo "${BUILD_DIR}/jurism/resource/config.js"
-		sed -si "s/%%VALUE%%/${CL_KEY}/" "${BUILD_DIR}/jurism/resource/config.js"
+		${GSED} -si "s/%%VALUE%%/${CL_KEY}/" "${BUILD_DIR}/jurism/resource/config.js"
 	    else
 	        LATEST=$(${GFIND} "$CONTAINER_DIR/${LOCAL_DIR}"/releases -type f -name '*.xpi' -printf '%AY%Am%Ad%AH%AI%AM%AS %h/%f\n' | sort -r | head -1 | cut -d\  -f 2);
                 unzip -q "${LATEST}" -d "$CALLDIR/modules/$LOCAL_DIR/"
